40502b9d93944a384d707b45c7b2f2dee59534fd
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-10-31 09:37:36 -0500

    Update bloque.h

--- session11a/bloque.h
+++ session11a/bloque.h
@@ -1,15 +1,11 @@
 #ifndef SESSION11A_BLOQUE_H
 #define SESSION11A_BLOQUE_H
 
-typedef int entero;
-typedef char caracter;
-typedef float decimal;
-typedef bool logico;
-
 class Bloque {
 public:
     bool es_mina;
     int minas;
-    bool flag;
+
 };
+#endif
 

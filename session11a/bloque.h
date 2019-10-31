307302c2b69c4f7df83766fb6156ce6709b0ef19
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-10-31 09:05:44 -0500

    Create bloque.h

---
+++ session11a/bloque.h
@@ -0,0 +1,15 @@
+#ifndef SESSION11A_BLOQUE_H
+#define SESSION11A_BLOQUE_H
+
+typedef int entero;
+typedef char caracter;
+typedef float decimal;
+typedef bool logico;
+
+class Bloque {
+public:
+    bool es_mina;
+    int minas;
+    bool flag;
+};
+

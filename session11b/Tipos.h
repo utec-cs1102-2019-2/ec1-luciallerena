58f0bce591d6d9a09ec506c250ceb2fdc8180ff0
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-10-31 10:13:04 -0500

    Create Tipos.h

---
+++ session11b/Tipos.h
@@ -0,0 +1,14 @@
+//
+// Created by lucia.llerena on 31/10/2019.
+//
+
+#ifndef TIPOS_H
+#define TIPOS_H
+#include <iostream>
+using namespace std;
+#include <iomanip>
+typedef int TipoEntero;
+typedef double TipoDecimal;
+typedef char TipoCaracter  ;
+typedef string TipoString;
+#endif

73725894642ddf3c20e91b9f14354f69056f689e
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-11-04 18:09:30 -0500

    Update Estado.h

--- session11b/Tarea/Estado.h
+++ session11b/Tarea/Estado.h
@@ -2,11 +2,23 @@
 // Created by lucia.llerena on 31/10/2019.
 //
 
+
+
+
 #ifndef HERENCIA_ESTADO_H
 #define HERENCIA_ESTADO_H
-
-
+#include <iomanip>
+#include <iostream>
+using namespace std;
 class Estado {
+private:
+    string Estado;
+    int Cantidad;
+public:
+    Estado(string, int);
+    ~Estado();
+    int getCantidad();
+    string getEstado();
 
 };
 

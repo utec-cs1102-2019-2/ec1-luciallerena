e75f6abba3c323ff1614d1f6526a12c0da1d9600
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-11-06 16:35:24 -0500

    Create Bicicleta.h

---
+++ session12a/Bicicleta.h
@@ -0,0 +1,15 @@
+//
+// Created by lucia.llerena on 6/11/2019.
+//
+
+#ifndef SESSION12A_BICICLETA_H
+#define SESSION12A_BICICLETA_H
+#include "Vehiculo.h"
+
+class Bicicleta:Vehiculo {
+public:
+    void avanzar();
+    void retroceder();
+    Bicicleta();
+};
+#endif //SESSION12A_BICICLETA_H

9b434e6d35891eb5b8674aa2b6c4bdacf4034a9c
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-11-06 15:32:28 -0500

    Update Mago.cpp

--- session11b/Tarea/Mago.cpp
+++ session11b/Tarea/Mago.cpp
@@ -1,18 +1,17 @@
-//
-// Created by lucia.llerena on 31/10/2019.
-//
-
 #include "Mago.h"
-Mago::Mago(int Alimento, int Cantidad, int estado){
-  
-    Alimento=Alimento;
-    Cantidad=Cantidad;
-    estado=estado;
+Mago::Mago(string pAlimento, int pCantidad, string pEstado){
+    Alimento=pAlimento;
+    Cantidad=pCantidad;
+    Estado=pEstado;
 }
+Mago::Mago(){}
 Mago::~Mago() {}
-int Mago::getAlimento(){
+string Mago::getAlimento(){
     return Alimento;
 }
 int Mago::getCantidad(){
     return Cantidad;
+}
+string Mago::getEstado(){
+    return Estado;
 }

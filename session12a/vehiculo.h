d8857b06acfce127a5970d8a5f308354ed557a0d
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-11-06 16:34:45 -0500

    Create vehiculo.h

---
+++ session12a/vehiculo.h
@@ -0,0 +1,19 @@
+
+
+#ifndef SESSION12A_VEHICULO_H
+#define SESSION12A_VEHICULO_H
+
+
+class Vehiculo {
+public:
+    int VelMax;
+    int VelMin;
+    int VelActual;
+    int x;
+    int y;
+    void avanzar();
+    void retroceder();
+};
+
+
+#endif //SESSION12A_VEHICULO_H

fa99e3d3e5ec4bea85a1a6e06ad5ff537c5c6f01
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-11-20 17:00:16 -0500

    Create Juego.h

---
+++ session14A/Juego.h
@@ -0,0 +1,18 @@
+//
+// Created by lucia.llerena on 20/11/2019.
+//
+
+#ifndef SESSION14A_JUEGO_H
+#define SESSION14A_JUEGO_H
+#include "Baraja.h"
+#include "Carta.h"
+
+class Juego {
+Baraja *b;
+vector<Carta>carta;
+public:
+    void start();
+};
+
+
+#endif //SESSION14A_JUEGO_H

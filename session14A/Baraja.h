11270a5be90bf73d65cf5e07fa89d29d7d6fe20e
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-11-20 17:00:40 -0500

    Create Baraja.h

---
+++ session14A/Baraja.h
@@ -0,0 +1,18 @@
+//
+// Created by lucia.llerena on 20/11/2019.
+//
+
+#ifndef SESSION14A_BARAJA_H
+#define SESSION14A_BARAJA_H
+#include "Palo.h"
+
+class Baraja {
+vector<Palo>palos;
+public:
+    Baraja();
+
+
+};
+
+
+#endif //SESSION14A_BARAJA_H

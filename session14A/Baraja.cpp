259ce7ded5c90d9502ec446070c87e028a0497b3
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-11-21 10:54:22 -0500

    Update Baraja.cpp

--- session14A/Baraja.cpp
+++ session14A/Baraja.cpp
@@ -2,5 +2,15 @@
 // Created by lucia.llerena on 20/11/2019.
 //
 
+#include <vector>
 #include "Baraja.h"
-Baraja::Baraja() {}
+Baraja::Baraja() {
+    Palo *corazones=new Palo("corazones","rojo");
+    Palo *trebol=new Palo("trebol","negro");
+    Palo *rombo=new Palo("rombo","rojo");
+    Palo *espada=new Palo("espada","negro");
+    palos.push_back(*trebol);
+    palos.push_back(*rombo);
+    palos.push_back(*corazones);
+    palos.push_back(*espada);
+}

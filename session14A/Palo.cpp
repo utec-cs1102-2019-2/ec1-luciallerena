b8eb1a33d6d7b2eb2f0765bf8d84ae79efb051cc
luciallerena <55211940+luciallerena@users.noreply.github.com>
2019-11-21 10:56:05 -0500

    Update Palo.cpp

--- session14A/Palo.cpp
+++ session14A/Palo.cpp
@@ -1,8 +1,14 @@
 //
 // Created by lucia.llerena on 20/11/2019.
 //
-
+#include "Carta.h"
 #include "Palo.h"
-void Palo::palos(string nombre, string color){}
-Palo::Palo(){}
-Palo::~Palo(){}
+Palo::Palo(string nombre,string color){
+    this->nombre=nombre;
+    this->color=color;
+    for(int i=0;i<14;i++){
+        Carta *carta=new Carta(i,nombre);
+        cartas.push_back(carta);
+    }
+}
+
